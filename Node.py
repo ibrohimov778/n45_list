79ce9ec861ad7bef84b61ef6cfa5a0e02cacbbf9
ibrohimov778 <sinyorstudent@gmail.com>
2024-06-03 15:49:36 +0300

    commit

--- Node.py
+++ Node.py
@@ -15,7 +15,7 @@ class LinkedList:
 
 
     def push(self, new_data):
-clear        new_node = Node(new_data)
+        new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
 
